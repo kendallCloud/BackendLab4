c0bb148df99c69dedccc8f15dfd27d3b6dfd2f40
kendallCloud <kendall24gb@gmail.com>
2022-05-12 20:54:13 -0600

    cierra sesion

--- app.ts
+++ app.ts
@@ -29,6 +29,7 @@ app.get('/',function (req:any, res:any) {
     "hola":"Bienvenido al servidor "+req.query.name
   }
   console.log("Bienvenido al servidor "+req.query.name);
+  session.userid=req.query.name;
   res.send(json);
   console.log("sesion iniciada");
 });
@@ -46,11 +47,11 @@ app.post('/user',(req:any,res:any) => {
 })
 
 app.get('/logout',(req:any,res:any) => {
+  var mensaje = new String(session.userid +" a cerrado sesion");
+  var json = {
+    "adios":mensaje
+  }
   req.session.destroy();
-  res.redirect('/');
-});
-
-app.get('/p', function (req:any,res:any) {
-  console.log("HOLA POST");
-  // console.log(JSON.stringify(req.body));
+  console.log(mensaje);
+  res.send(mensaje);
 });
